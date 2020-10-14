82cf7fc7ffc13133285f90daca81d8e91c20d2ed
Gabriel Luiz <gabrielluiz@msn.com>
2020-10-14 19:44:50 -0300

    Add files via upload

---
+++ Script de adicionar computadores a um grupo do Active Directory usando um arquivo TXT.ps1
@@ -0,0 +1,25 @@
+﻿## Script de adicionar computadores a um grupo do Active Directory usando um arquivo TXT - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+# Obtenha o conteúdo do arquivo e armazene-o em uma variável chamada $ List
+
+$List=Get-Content C:\Scripts\Computer.txt
+
+
+# Adiciona os objetos computadores contidos no arquivo TXT, com os hostnames dos computadores ao Grupo Active Directory escolhido.
+
+$List | foreach {Add-ADGroupMember -id VPN -MEMBERS (Get-ADComputer $_) }
+
+<#
+
+Referências:
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-content?view=powershell-7/?WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/foreach-object?view=powershell-7/?WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/addsadministration/add-adgroupmember?view=win10-ps/?WT.mc_id=WDIT-MVP-5003815
+
+https://docs.microsoft.com/en-us/powershell/module/addsadministration/get-adcomputer?view=win10-ps/?WT.mc_id=WDIT-MVP-5003815
+
+#>
---
+++ Script de levantamento de todos os objetos computadores do Active Directory.ps1
@@ -0,0 +1,13 @@
+﻿## Script de levantamento de todos os objetos computadores do Active Directory - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+
+Get-ADComputer -Filter * -Properties Name, PasswordLastSet, OperatingSystem, DNSHostName, DistinguishedName, Enabled | select Name, PasswordLastSet, OperatingSystem, DNSHostName, DistinguishedName, Enabled | Export-Csv "c:\Scripts\todososcomputadores.csv" -Encoding UTF8
+
+
+<#
+
+Referência:
+
+https://docs.microsoft.com/en-us/powershell/module/addsadministration/get-adcomputer?view=win10-ps/?WT.mc_id=WDIT-MVP-5003815
+
+#> 
